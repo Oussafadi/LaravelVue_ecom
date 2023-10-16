ea51a32da05bebeddf7091b43cf6b4b5fbb1a8f8
Oussafadi <fadi.oussa@outlook.fr>
2023-10-16 01:06:38 +0100

    products listing show and vue components

---
+++ resources/js/components/AddToCart.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex item-center justify-end py-4">
+        <button  class="bg-yellow-400 text-red-800 p-2 font-bold"  v-on:click.prevent="addToCart"> Add to Cart</button>
+    </div>
+</template>
+
+<script setup>
+
+const addToCart = () => console.log('added');
+</script>
+
